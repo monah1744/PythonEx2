82c44b1770e89fd7231693af678dfa0a640a0d95
Ivan Novikov <monah1744@gmail.com>
2019-11-05 20:09:10 +0200

    HW-2 Ex-2

--- HW_2/Ex_2.py
+++ HW_2/Ex_2.py
@@ -1,1 +1,13 @@
-print()
+my_number = int(input("intput number between 1-5 : "))
+if my_number == 1:
+    print("true - it's 1")
+elif my_number == 2:
+    print("true - it's 2")
+elif my_number == 3:
+    print("true - it's 3")
+elif my_number == 4:
+    print("true - it's 4")
+elif my_number == 5:
+    print("true - it's 5")
+else:
+    print("false - it isn't 1-5")
