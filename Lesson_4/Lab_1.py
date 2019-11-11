cb64996c59ac607fd1401bcf522d9b9a0dd7af93
Ivan Novikov <monah1744@gmail.com>
2019-11-11 23:51:28 +0200

    working option - latest version

--- Lesson_4/Lab_1.py
+++ Lesson_4/Lab_1.py
@@ -1,13 +1,15 @@
 import sys
 import random
+
 frases = [", yeah!", ", this is crazy, I tell ya.", ", can U believe this?",
           ", eh?", ", aw yea.", ", yo.", " No way!", ". Awesome!"]
 with open(sys.argv[1], 'r') as file_read:
     for file_line in file_read:
-        file_line.rstrip()
-        print(file_line)
-        li = [x for x in file_line.split(".")]
-        print(li)
-        for sentence in li:
-            ##sentence += frases[random.randint(0, len(frases)-1)]
-            print(sentence, end=(" "))
+        end_li = len(file_line)
+        while file_line.rfind(".", 0, end_li) >= 0:
+            finded_index = file_line.rfind(".", 0, end_li)
+            file_line = file_line[:finded_index] + \
+                frases[random.randint(0, len(frases)-1)] + \
+                file_line[(finded_index+1):]
+            end_li = finded_index-1 if finded_index else 0
+        print(file_line, end="")
