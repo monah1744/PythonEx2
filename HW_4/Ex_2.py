14116fb324774d081b0c38e09c591a3d9ba4777b
Ivan Novikov <monah1744@gmail.com>
2019-11-13 23:32:14 +0200

    HW_4 Ex_2

---
+++ HW_4/Ex_2.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def check_all(my_file, my_fizz, my_buzz, my_counter):
+    my_file.write(check_fizz(my_fizz, my_counter))
+    my_file.write(check_buzz(my_buzz, my_counter))
+    my_file.write(check_counter(my_fizz, my_buzz, my_counter))
+    my_file.write(" ")
+
+
+def check_buzz(num_buzz, num_check):
+    return "B" if num_check % num_buzz == 0 else ""
+
+
+def check_fizz(num_fizz, num_check):
+    return "F" if (not num_check % num_fizz) else ""
+
+
+def check_counter(num_fizz, num_buzz, num_check):
+    return(str(num_check)) if not num_check % num_buzz == 0 and num_check % num_fizz else ""
+
+
+try:
+    with open(sys.argv[1], 'r') as file_read:
+        try:
+            with open(sys.argv[2], 'w') as file_write:
+                for line in file_read:
+                    li = [int(x) for x in line.split()]
+                    counters = range(1, (li[2]+1))
+                    [check_all(file_write, li[0], li[1], counter)
+                     for counter in counters]
+                    file_write.write("\n")
+        except IOError:
+            print("An IOError has occurred with ", sys.argv[2])
+except IOError:
+    print("An IOError has occurred with ", sys.argv[1])
+except IndexError:
+    print("not enough arguments! Would be better if we have two :)")
