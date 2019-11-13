32778bc276d8d929ef78395d2be43a24cc83d214
Ivan Novikov <monah1744@gmail.com>
2019-11-13 13:47:42 +0200

    tune

--- Lesson_4/Lab_1.py
+++ Lesson_4/Lab_1.py
@@ -8,8 +8,7 @@ with open(sys.argv[1], 'r') as file_read:
         end_li = len(file_line)
         while file_line.rfind(".", 0, end_li) >= 0:
             finded_index = file_line.rfind(".", 0, end_li)
-            file_line = file_line[:finded_index] + \
-                frases[random.randint(0, len(frases)-1)] + \
-                file_line[(finded_index+1):]
+            file_line = file_line[:finded_index] + frases[random.randint(
+                0, len(frases)-1)] + file_line[(finded_index+1):]
             end_li = finded_index-1 if finded_index else 0
         print(file_line, end="")
