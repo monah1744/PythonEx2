9aa13c200fda7d30eb2c4ba17db642088c51cb58
Ivan Novikov <monah1744@gmail.com>
2019-11-20 19:55:01 +0200

    first commit

---
+++ Module_1/first_test.py
@@ -0,0 +1,75 @@
+def spam(number):
+    '''Function should return something like this:
+    spam(1);#bulochka
+    spam(3);#bulochkabulochkabulochka
+    But it is broken. Fix it please!
+
+    Эта функция принимает числовой параметр. Должна вернуть строку, которая
+    повторяется столько раз, сколько параметров передано. Она уже написана,
+    но не работает. Любым способом заставьте ее работать.
+    '''
+    return str('bulochka'*number)
+
+
+def my_sum(list_of_numbers):
+    """Function receives a list with integer numbers,
+    should return its sum as an integer. Do not use built in summarize functions.
+    :param list
+
+    Функция получает на вход массив чисел, должна вернуть сумму этих чисел.
+    Не использовать встроенные функции суммирования.
+
+    """
+    pass
+    my_summ = 0
+    for my_numm in list_of_numbers:
+        my_summ += my_numm
+    return my_summ
+    #  ...wite your code here
+
+
+def shortener(string):
+    """
+    Function receives a long string with many words.
+    It should return the same string, but words,
+    larger then 6 symbols should be changed, symbols
+    after the sixth one should be replaced by symbol *
+    :param string
+    :returns string
+
+     Функция получает на вход длинную строку с множеством слов.
+     Она должна вернуть ту же строку, но в словах, которые длиннее 6 символов,
+     функция должна вместо всех символов после шестого поставить одну звездочку.
+     Пример: Из слова 'verwijdering' должно получиться 'verwij*'
+    """
+    pass
+    my_new_string = ""
+    for my_string in string.split():
+        if len(my_string) > 6:
+            my_new_string = my_new_string + my_string[:6] + "* "
+        else:
+            my_new_string = my_new_string + my_string + " "
+    return my_new_string.rstrip()
+    #  ...wite your code here
+
+
+def compare_ends(words):
+    """
+    Function receives an array of strings.
+    Please return number of strings, which
+    length is at least 2 symbols and first character
+    is equal to the last character
+
+    Функция получает на вход массив строк. Вернуть надо количество строк,
+    которые не короче двух символов и у которых первый и последний
+    символ совпадают.
+
+    """
+    pass
+    counter = 0
+    for my_word in words:
+        if len(my_word) >= 2:
+            if my_word[0] == my_word[len(my_word)-1]:
+                counter += 1
+    return counter
+    #  ...wite your code here
