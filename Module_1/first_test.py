e4381237edd3812333add3c8796bce96b5939a54
Ivan Novikov <monah1744@gmail.com>
2019-11-20 23:37:12 +0200

    tune code

--- Module_1/first_test.py
+++ Module_1/first_test.py
@@ -1,25 +1,9 @@
 def spam(number):
-    '''Function should return something like this:
-    spam(1);#bulochka
-    spam(3);#bulochkabulochkabulochka
-    But it is broken. Fix it please!
-
-    Эта функция принимает числовой параметр. Должна вернуть строку, которая
-    повторяется столько раз, сколько параметров передано. Она уже написана,
-    но не работает. Любым способом заставьте ее работать.
-    '''
-    return str('bulochka'*number)
+    res = "".join(['bulochka' for i in range(number)])
+    return res
 
 
 def my_sum(list_of_numbers):
-    """Function receives a list with integer numbers,
-    should return its sum as an integer. Do not use built in summarize functions.
-    :param list
-
-    Функция получает на вход массив чисел, должна вернуть сумму этих чисел.
-    Не использовать встроенные функции суммирования.
-
-    """
     my_summ = 0
     for my_numm in list_of_numbers:
         my_summ += my_numm
@@ -27,19 +11,6 @@ def my_sum(list_of_numbers):
 
 
 def shortener(string):
-    """
-    Function receives a long string with many words.
-    It should return the same string, but words,
-    larger then 6 symbols should be changed, symbols
-    after the sixth one should be replaced by symbol *
-    :param string
-    :returns string
-
-     Функция получает на вход длинную строку с множеством слов.
-     Она должна вернуть ту же строку, но в словах, которые длиннее 6 символов,
-     функция должна вместо всех символов после шестого поставить одну звездочку.
-     Пример: Из слова 'verwijdering' должно получиться 'verwij*'
-    """
     my_new_string = ""
     for my_string in string.split():
         if len(my_string) > 6:
@@ -50,20 +21,6 @@ def shortener(string):
 
 
 def compare_ends(words):
-    """
-    Function receives an array of strings.
-    Please return number of strings, which
-    length is at least 2 symbols and first character
-    is equal to the last character
-
-    Функция получает на вход массив строк. Вернуть надо количество строк,
-    которые не короче двух символов и у которых первый и последний
-    символ совпадают.
-
-    """
-    pass
-    counter = 0
-    for my_word in words:
-        if len(my_word) >= 2 and my_word[0] == my_word[len(my_word)-1]:
-            counter += 1
-    return counter
+    counter = [1 for my_word in words if len(
+        my_word) >= 2 and my_word[0] == my_word[len(my_word)-1]]
+    return len(counter)
