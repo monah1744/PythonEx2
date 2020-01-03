e419669988d129ec12a2e798765b9082d819bfab
Ivan Novikov <monah1744@gmail.com>
2020-01-03 19:32:16 +0200

    autopep

--- Python_OOP_3 (Completed)/app.py
+++ Python_OOP_3 (Completed)/app.py
@@ -11,12 +11,12 @@ second_P.tech_stack.append("PHP")
 
 first_R = Recruiter("Marina Laktionova", "email2@gmail.com", "050", 20)
 
-first_C = Candidate("Dmitriy Shmatko", "dshamtko@gmail.com", [
-                    "Lua", "Python", "Puppet"], ["Lua"], 10)
-second_C = Candidate("Anatoliy Laktionov", "alaktionov@gmail.com", [
-                     "Lua", "Python", "Puppet"], ["Python"], 5)
-third_C = Candidate("Igor Vnukov", "ivnukov@gmail.com", [
-                    "Lua", "Python", "PHP"], ["Python"], 10)
+first_C = Candidate("Dmitriy Shmatko", "dshamtko@gmail.com",
+                    ["Lua", "Python", "Puppet"], ["Lua"], 10)
+second_C = Candidate("Anatoliy Laktionov", "alaktionov@gmail.com",
+                     ["Lua", "Python", "Puppet"], ["Python"], 5)
+third_C = Candidate("Igor Vnukov", "ivnukov@gmail.com",
+                    ["Lua", "Python", "PHP"], ["Python"], 10)
 
 first_V = Vacancy("Middle Python", "Python", 5)
 second_V = Vacancy("Senior Python", "Python", 8)
