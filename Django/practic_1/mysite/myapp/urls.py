1c3121af8f776f7c7ce5dccb00e5a45b819a9477
Ivan Novikov <monah1744@gmail.com>
2020-02-18 15:54:20 +0200

    django

--- Django/practic_1/mysite/myapp/urls.py
+++ Django/practic_1/mysite/myapp/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('user', views.user),
+    path('', views.hello, name='hello'),
+    path('user', views.user, name='user'),
+    path('index1', views.index1, name='index1'),
+    path('index2', views.index2, name='index2'),
+
 ]
--- Django/practic_1/mysite/mysite/settings.py
+++ Django/practic_1/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cj=_(3dmpshqhz^v-3)q#l&bn(9@w)y7@c9*-1&yev=e0ns+lc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kuzma', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
