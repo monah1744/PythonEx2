08750de5a730a36549186c745ee1ed9af9e10543
Ivan Novikov <monah1744@gmail.com>
2020-02-18 16:50:22 +0200

    for tests

---
+++ for_test.py
@@ -0,0 +1,4 @@
+s = {1: "winter", 2: "spring", 3: "summer", 4: "autumn"}
+m = {1: 1, 2: 1, 3: 2, 4: 2, 5: 2, 6: 3, 7: 3, 8: 3, 9: 4, 10: 4, 11: 4, 12: 1}
+mm = int(input("enter mounth : "))
+print(s[m[mm]])
